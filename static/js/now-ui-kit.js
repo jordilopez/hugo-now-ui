f24c8ab654d613858ce1b70b041011064db2190a
Jordi López <voodoo76@gmail.com>
2017-12-17 01:01:11 +0100

    Prevent errors when creating noUiSlider when there are no sliders on the markup

--- static/js/now-ui-kit.js
+++ static/js/now-ui-kit.js
@@ -175,25 +175,30 @@ nowuiKit = {
         // Sliders for demo purpose in refine cards section
         var slider = document.getElementById('sliderRegular');
 
-        noUiSlider.create(slider, {
-            start: 40,
-            connect: [true, false],
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
+        if (slider) {
+            noUiSlider.create(slider, {
+                start: 40,
+                connect: [true, false],
+                range: {
+                    min: 0,
+                    max: 100
+                }
+            });
+        }
+
 
         var slider2 = document.getElementById('sliderDouble');
 
-        noUiSlider.create(slider2, {
-            start: [20, 60],
-            connect: true,
-            range: {
-                min: 0,
-                max: 100
-            }
-        });
+        if (slider2) {
+            noUiSlider.create(slider2, {
+                start: [20, 60],
+                connect: true,
+                range: {
+                    min: 0,
+                    max: 100
+                }
+            });
+        }
     }
 }
 
